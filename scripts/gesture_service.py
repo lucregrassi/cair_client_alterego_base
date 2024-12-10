20db925781e701ff85f54952345b861b6796928a
Lucrezia <lucre.grassi@gmail.com>
2024-12-10 17:43:13 +0100

    renamed node

--- scripts/gesture_service.py
+++ scripts/gesture_service.py
@@ -23,19 +23,24 @@ def handle_gesture_service(req):
     info_dict = yaml.load(Bag(filename, 'r')._get_yaml_info())
     rosbag_duration = info_dict["duration"]
 
-    remaining_time = audio_duration
-    while remaining_time > 0:
-        if remaining_time < (rosbag_duration - offset):
-            play_time = remaining_time
-        else:
-            play_time = rosbag_duration - offset
-        os.system("rosbag play -s {} -u {} {}".format(offset, play_time, filename))
-        remaining_time = remaining_time - play_time
-        offset = (offset + play_time) % rosbag_duration
+    if audio_duration == 0:
+        os.system("rosbag play {}".format(filename))
+        rospy.loginfo("Gesture completed")
+        return 0
+    else:
+        remaining_time = audio_duration
+        while remaining_time > 0:
+            if remaining_time < (rosbag_duration - offset):
+                play_time = remaining_time
+            else:
+                play_time = rosbag_duration - offset
+            os.system("rosbag play -s {} -u {} {}".format(offset, play_time, filename))
+            remaining_time = remaining_time - play_time
+            offset = (offset + play_time) % rosbag_duration
 
-    rospy.loginfo("Server processing complete: new offset %f", offset)
-    # The server returns a boolean
-    return offset
+        rospy.loginfo("Gesture completed: new offset %f", offset)
+        # The server returns a boolean
+        return offset
 
 
 def gesture_service_server():
